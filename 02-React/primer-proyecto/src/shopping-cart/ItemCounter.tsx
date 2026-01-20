357c3a15b59de76bbf1f064572eb6bf5e1c3483c
MikeBrevis <michel.brevis@gmail.com>
2026-01-20 12:36:05 -0300

    fin de primeros pasos en react

--- 02-React/primer-proyecto/src/shopping-cart/ItemCounter.tsx
+++ 02-React/primer-proyecto/src/shopping-cart/ItemCounter.tsx
@@ -1,47 +1,37 @@
 import { useState } from "react";
+import './ItemCounter.css';
 
 interface Props {
     name: string;
-    quantity? : number;
+    quantity?: number;
 }
 
-export const  ItemCounter = ({ name, quantity = 1 }: Props) => {
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
 
-    const [ count, setCount ] =  useState(quantity);
+    const [count, setCount] = useState(quantity);
 
     const handleAdd = () => {
-        setCount( count + 1 );
+        setCount(count + 1);
     }
     const handleRemove = () => {
-        if ( count === 0 ) return;
-        setCount( count - 1 );
+        if (count === 0) return;
+        setCount(count - 1);
     }
 
-/* rafc + tab = Atajo para crear un componente funcional */
-        return (
-            <section style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '1rem',
-                backgroundColor: 'red',
-            }}
-            >
-                <span style={{
-                    width: '200px',
-                    fontWeight: 'bold',
-                    fontSize: '1.5rem',
-                    margin: '10px'
-                }}
-                >
-                    { name }
-                </span>
+    /* rafc + tab = Atajo para crear un componente funcional */
+    return (
+        <section className="style-row">
+            <span className="item-row">
+                {name}
+            </span>
+            <button onClick={handleAdd}>+</button>
+            <span>{count}</span>
+            <button onClick={handleRemove}>-</button>
+        </section>
+    )
+}
 
-                <button onClick={handleAdd}>+</button>
-                <span>{ count }</span>
-                <button onClick={handleRemove}>-</button>
-            </section>
-        )
-    }
+
 
 
 
