d77b9c4def158410f2cae797bb1da213c729d0f0
MikeBrevis <michel.brevis@gmail.com>
2026-02-20 16:48:29 -0300

    actualizacion fundamentos -desarrollo de software

--- 00-Fundamentos_programacion/fundamentos_programacion.js
+++ 00-Fundamentos_programacion/fundamentos_programacion.js
@@ -1,2 +1,17 @@
 // Lenguajes de alto, bajo nivel y compilación 
 
+/* 
+Los lenguajes de bajo nivel de compilacion estan muy cerca del hardware. Dicen especificamente que mover y donde , usando instrucciones que la maquina entiende directamente. 
+
+Los lenguajes de alto nivel son mas abstractos y se centran en la comprencion humana y en la logica del programa, no en los detalles del hardware.
+
+Los lenguajes de alto nivel se compilan a lenguaje de bajo nivel para que la maquina pueda entenderlos. 
+*/
+
+// Video 7 == Desarrollo del software
+
+/* 
+# Analisis, donde nace la idea.
+*/
+
+
